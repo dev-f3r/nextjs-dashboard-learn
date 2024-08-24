783ce0112614fa59afae44104d864e21d05d29cf
Fernando Castro <devfercastro@gmail.com>
2024-08-23 22:39:39 -0300

    fix(login): Lol

--- auth.ts
+++ auth.ts
@@ -31,6 +31,8 @@ export const { auth, signIn, signOut } = NextAuth({
           if (!user) return null;
 
           const passwordsMatch = await bcrypt.compare(password, user.password);
+
+          if (passwordsMatch) return user;
         }
 
         console.log('Invalid credentials');
